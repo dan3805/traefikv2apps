97f9e36c1b0157cc51dcbc5f3995bf622a89b8ef
doob187 <amderkum@gmail.com>
2021-03-15 23:27:29 +0100

    push

--- install.sh
+++ install.sh
@@ -12,8 +12,8 @@ EOF
 exit 0
 fi
 while true; do
-  if [[ ! -x "$(command -v docker)"  ]]; then exit 0;fi
-  if [[ ! -x "$(command -v docker-compose)" ]]; then exit 0;fi
+  if [[ ! -x $(command -v docker)  ]]; then exit 0;fi
+  if [[ ! -x $(command -v docker-compose) ]]; then exit 0;fi
   interface
 done
 }
@@ -71,7 +71,7 @@ install
 }
 
 install() {
-buildshow="ls /opt/apps/${section}"
+buildshow="ls /opt/apps/${section}/compose/"
 build=$($buildshow | sed -e 's/.yml//g' )
 if [[ $section == "mediaserver" ]]; then
     buildup=$($buildshow | sed -e 's/.yml//g' | sort -r | sed -e 's/embyintel//g' | sed -e 's/embynplex//g' | sed -e 's/plexnvidia//g' | sed -e 's/plexintel//g')
@@ -95,7 +95,7 @@ EOF
   read -p '↪️ Type app to install | Press [ENTER]: ' typed </dev/tty
   if [[ $typed == "exit" || $typed == "Exit" || $typed == "EXIT" || $typed == "z" || $typed == "Z" ]]; then exit; fi
   if [[ $typed == "" ]]; then install; fi
-  current=$(ls /opt/apps/${section}/ | sed -e 's/.yml//g' | grep -qE "\<$typed\>" 1>/dev/null 2>&1 && echo true || echo false)
+  current=$(ls /opt/apps/${section}/compose/ | sed -e 's/.yml//g' | grep -qE "\<$typed\>" 1>/dev/null 2>&1 && echo true || echo false)
   if [[ $current == "true" && $typed != "" ]]; then run;fi
 
 }
@@ -103,29 +103,29 @@ run() {
 compose="compose/docker-compose.yml"
 appfolder="/opt/apps"
 basefolder="/opt/appdata"
- if [[ ! -d $basefolder/compose/ ]];then mkdir -p $basefolder/compose/; fi
- if [[ ! -x "$(command -v rsync)" ]]; then apt install rsync -yqq >/dev/null 2>&1; fi
+ if [[ ! -d $basefolder/compose/ ]];then $(command -v mkidr) -p $basefolder/compose/; fi
+ if [[ ! -x $(command -v rsync) ]]; then $(command -v apt) install rsync -yqq >/dev/null 2>&1; fi
  if [[ ${section} == "mediaserver" ]]; then
     if [[ ${typed} == "emby" || ${typed} == "plex" ]]; then
        IGPU=$(lshw -C video | grep -qE 'i915' && echo true || echo false)
        NGPU=$(lshw -C video | grep -qE 'nvidia' && echo true || echo false)
        if [[ $IGPU == "true" ]]; then
-          rsync $appfolder/${section}/${typed}intel.yml $basefolder/$compose -aq --info=progress2 -hv
+          $(command -v rsync) $appfolder/${section}/compose/${typed}intel.yml $basefolder/$compose -aq --info=progress2 -hv
        elif [[ $NGPU == "true" ]]; then
-          rsync $appfolder/${section}/${typed}nvidia.yml $basefolder/$compose -aq --info=progress2 -hv
+          $(command -v rsync) $appfolder/${section}/compose/${typed}nvidia.yml $basefolder/$compose -aq --info=progress2 -hv
        else
-          rsync $appfolder/${section}/${typed}.yml $basefolder/$compose -aq --info=progress2 -hv
+          $(command -v rsync) $appfolder/${section}/compose/${typed}.yml $basefolder/$compose -aq --info=progress2 -hv
        fi
     fi
  else
-   rsync $appfolder/${section}/${typed}.yml $basefolder/$compose -aq --info=progress2 -hv
+   $(command -v rsync) $appfolder/${section}/compose/${typed}.yml $basefolder/$compose -aq --info=progress2 -hv
 fi
  if [[ ! -d $basefolder/${typed} ]]; then
     folder=$basefolder/${typed}
     for i in ${folder}; do
-        mkdir -p $i
-        find $i -exec chmod a=rx,u+w {} \;
-        find $i -exec chown -hR 1000:1000 {} \;
+        $(command -v mkidr) -p $i
+        $(command -v find) $i -exec $(command -v chmod) a=rx,u+w {} \;
+        $(command -v find) $i -exec $(command -v chown) -hR 1000:1000 {} \;
     done
  fi
 ## change values inside docker-compose.yml
@@ -158,17 +158,19 @@ if [[ $TZTEST != "false" ]]; then
       fi
    fi
 fi
-container=$(docker ps -aq --format '{{.Names}}' | sed '/^$/d' | grep -E "\<$typed\>")
+container=$($(command -v docker) ps -aq --format '{{.Names}}' | sed '/^$/d' | grep -E "\<$typed\>")
 if [[ $container != "" ]]; then
-   docker stop $container 1>/dev/null 2>&1
-   docker rm $container 1>/dev/null 2>&1
-   docker image prune -af 1>/dev/null 2>&1
+   docker="stop rm"
+   for i in ${docker}; do
+       $(command -v docker) $i $container 1>/dev/null 2>&1
+   done
+   $(command -v docker) image prune -af 1>/dev/null 2>&1
 else
-   docker image prune -af 1>/dev/null 2>&1
+   $(command -v docker) image prune -af 1>/dev/null 2>&1
 fi
 if [[ ${section} == "mediaserver" && ${typed} == "plex" ]]; then plexclaim;fi
-cd $basefolder/compose/ && docker-compose up -d --force-recreate 1>/dev/null 2>&1
-if [[ ${section} == "mediaserver" && ${typed} == "plex" ]]; then plexprefrences;fi
+   cd $basefolder/compose/ && $(command -v docker-compose) up -d --force-recreate 1>/dev/null 2>&1
+if [[ ${section} == "mediaserver" || ${section} == "downloadclients" ]]; then subtasks;fi
 }
 plexclaim() {
   tee <<-EOF
@@ -200,55 +202,20 @@ else
   plexclaim
 fi
 }
-plexprefrences() {
-xmltest=$(command -v xmlstarlet)
-if [[ ! -x $xmltest ]]; then apt install xmlstarlet -yqq; fi
-DOMAIN=$(cat /etc/hosts | grep 127.0.0.1 | tail -n 1 | awk '{print $2}')
-SERVERIP=$(ip addr show |grep 'inet '|grep -v 127.0.0.1 |awk '{print $2}'| cut -d/ -f1 | head -n1)
-prefsecure=$(cat "/opt/appdata/plex/database/Library/Application Support/Plex Media Server/Preferences.xml" | grep -qE secureConnections && echo true || echo false)
-prefconnection=$(cat "/opt/appdata/plex/database/Library/Application Support/Plex Media Server/Preferences.xml" | grep -qE customConnections && echo true || echo false)
-if [[ $prefconnection == "false" || $prefsecure == "false" ]]; then
-  echo "Adding resource Preferences.xml"
-  replace="xmlstarlet"
-  file="/opt/appdata/plex/database/Library/Application Support/Plex Media Server/Preferences.xml"
-  for i in ${replace}; do
-    $i ed -O --inplace --insert "/Preferences" --type attr -n "secureConnections" -v "1" $file
-    $i ed -O --inplace --insert "/Preferences" --type attr -n "GenerateChapterThumbBehavior" -v "never" $file
-    $i ed -O --inplace --insert "/Preferences" --type attr -n "LoudnessAnalysisBehavior" -v "never" $file
-    $i ed -O --inplace --insert "/Preferences" --type attr -n "MinutesAllowedPaused" -v "10" $file
-    $i ed -O --inplace --insert "/Preferences" --type attr -n "customConnections" -v "http://$SERVERIP:32400, http://plex.$DOMAIN:32400" $file
-    $i ed -O --inplace --insert "/Preferences" --type attr -n "AcceptedEULA" -v "1" $file
-    $i ed -O --inplace --insert "/Preferences" --type attr -n "DlnaEnabled" -v "p" $file
-    $i ed -O --inplace --insert "/Preferences" --type attr -n "ScannerLowPriority" -v "1" $file
-    $i ed -O --inplace --insert "/Preferences" --type attr -n "ManualPortMappingMode" -v "1" $file
-    $i ed -O --inplace --insert "/Preferences" --type attr -n "DlnaReportTimeline" -v "0" $file
-    $i ed -O --inplace --insert "/Preferences" --type attr -n "TranscoderTempDirectory" -v "/transcode" $file
-    $i ed -O --inplace --insert "/Preferences" --type attr -n "TranscoderThrottleBuffer" -v "200" $file
-    $i ed -O --inplace --insert "/Preferences" --type attr -n "TranscodeCountLimit" -v "4" $file
-    $i ed -O --inplace --insert "/Preferences" --type attr -n "autoEmptyTrash" -v "1" $file
-    $i ed -O --inplace --insert "/Preferences" --type attr -n "RelayEnabled" -v "0" $file
-    $i ed -O --inplace --insert "/Preferences" --type attr -n "DisableTLSv1_0" -v "1" $file
-    $i ed -O --inplace --insert "/Preferences" --type attr -n "TranscoderH264BackgroundPreset" -v "faster" $file
-    gpu="i915 nvidia"
-    for ii in ${gpu}; do
-       TDV=$(lshw -C video | grep -qE $ii && echo true || echo false)
-       if [[ $TDV == "true" ]]; then
-         $i ed -O --inplace --insert "/Preferences" --type attr -n "HardwareAcceleratedCodecs" -v "1" $file
-       fi
-    done
-  done
+subtasks() {
+if [[ -x $(command -v ansible) && -x $(command -v ansible-playbook) ]]; then
+   if [[ -f $appfolder/subactions/${typed}.yml ]] $(command -v ansible-playbook) $appfolder/subactions/${typed}.yml;fi
 fi
-container=$(docker ps -aq --format '{{.Names}}' | sed '/^$/d' | grep -E 'plex')
-if [[ $container == "plex" ]]; then
-   docker restart $container 1>/dev/null 2>&1
-fi
+#if [[ ${section} == "mediaserver" && ${typed} == "plex" || ${typed} == "emby" ]]; then $(command -v bash) $appfolder/subactions/${typed}.sh;fi
+container=$($(command -v docker) ps -aq --format '{{.Names}}' | sed '/^$/d' | grep -qE "\<$typed\>")
+if [[ ${section} == "mediaserver" || ${section} == "downloadclients" ]]; then $(command -v docker) restart $container 1>/dev/null 2>&1;fi
 }
 backupcomposer() {
 ## run autocomposer when all is done
-if [[ ! -d $basefolder/composebackup ]]; then mkdir -p $basefolder/composebackup/; fi
-docker=$(docker ps -aq --format {{.Names}} )
-docker run --rm -v /var/run/docker.sock:/var/run/docker.sock red5d/docker-autocompose $docker >>$basefolder/composebackup/docker-compose.yml
-docker system prune -af
+if [[ ! -d $basefolder/composebackup ]]; then $(command -v mkidr) -p $basefolder/composebackup/;fi
+docker=$($(command -v docker) ps -aq --format {{.Names}} )
+$(command -v docker) run --rm -v /var/run/docker.sock:/var/run/docker.sock red5d/docker-autocompose $docker >>$basefolder/composebackup/docker-compose.yml
+$(command -v docker) system prune -af
 }
 
 
@@ -258,8 +225,6 @@ docker system prune -af
 
 # Subactions 2
 #  >> downloader needs replacing after deploy for the configuration
-#  >> restart docker then when downloader is found
-
-#  >> run sqlite3 edit command
+#  >> restart $(command -v docker) then when downloader is found
 
 updatesystem
