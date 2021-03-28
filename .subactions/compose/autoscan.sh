a00863a06c90915516b4138849e8f3655daf7d81
doob187 <amderkum@gmail.com>
2021-03-28 19:09:55 +0200

    Update autoscan.sh

--- .subactions/compose/autoscan.sh
+++ .subactions/compose/autoscan.sh
@@ -152,7 +152,7 @@ tee <<-EOF
 ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 
 EOF
-   read -esp "Enter a password for autoscan " $PASSWORD
+   read -ep "Enter a password for autoscan " $PASSWORD
 
 if [[ $PASSWORD != "" ]]; then
    if [[ $(uname) == "Darwin" ]]; then
