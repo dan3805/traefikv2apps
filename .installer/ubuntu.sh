3cd455a2182ee8373dd337643e3add22f16302b7
doob187 <60312740+doob187@users.noreply.github.com>
2021-04-05 01:29:37 +0200

    Update ubuntu.sh

--- .installer/ubuntu.sh
+++ .installer/ubuntu.sh
@@ -274,7 +274,7 @@ EOF
      done
   fi
   if [[ ${section} == "system" && ${typed} == "mount" ]];then
-     checkmt=$($(command -v mountpoint) -q /mnt/unionfs && echo true || echo false)
+     checkmnt=$($(command -v mountpoint) -q /mnt/unionfs && echo true || echo false)
      mount=$($(command -v docker) ps -aq --format={{.Names}} | grep -x 'mount')
      if [[ ${checkmnt} == "true" && ${mount} == "mount" ]];then $(command -v docker) stop mount 1>/dev/null 2>&1 && $(command -v fusermount) -uzq /mnt/unionfs 1>/dev/null 2>&1;fi
      if [[ ${checkmnt} == "false" && ${mount} == "mount" ]];then $(command -v docker) stop mount 1>/dev/null 2>&1 && $(command -v fusermount) -uzq /mnt/unionfs 1>/dev/null 2>&1;fi
